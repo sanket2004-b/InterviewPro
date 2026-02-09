aa753d08f562216188dc672fd72caa9071acd279
Sanket <sanketmarkand7676@gmail.com>
2026-02-10 00:04:28 +0530

    frontend started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import "./index.css";
 
 
 import { SignIn, SignOutButton, SignedIn, SignedOut } from '@clerk/clerk-react'
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello From clerk authentication</h1>
+      <h1 className='text-red-500'>Hello From clerk authentication</h1>
       <SignedOut>
         <button>Login</button>
         <SignIn />
