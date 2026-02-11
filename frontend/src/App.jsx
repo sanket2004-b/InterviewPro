b2a0f9959677b3dcdebd31f55204c39b42d145aa
Sanket <sanketmarkand7676@gmail.com>
2026-02-11 22:24:16 +0530

    problemsPage added with data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./index.css";
 import {useUser} from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+// import dashboard from "./pages/dashboard";  
+import ProblemsPage from "./pages/ProblemsPage";
 
 function App() {
  
@@ -15,7 +17,8 @@ function App() {
   return (
     <>
     <Routes>
-       <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/"} />} />
+       <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to=<dashboard/> />} />
+       <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
     </Routes>
     </>
     
---
+++ frontend/src/pages/dashboard.jsx
@@ -0,0 +1,6 @@
+function dashboard(){
+    return(
+        <h1>your alerady login</h1>
+    )
+}
+export default dashboard;
